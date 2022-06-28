d8a3edc57caf92a499a9d16c36cbe0fd0cee5c1c
Gavin <guowenlong20000@sina.com>
2022-06-28 11:08:43 +0800

    create project

---
+++ example/android/app/src/main/kotlin/io/github/guowenlong/sample/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.guowenlong.sample.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/multitype.dart
@@ -0,0 +1,5 @@
+/// * Description: 入口
+/// * Author:      郭文龙
+/// * Date:        2022/6/28 11:02
+/// * Email:       guowenlong20000@sina.com
+library get;
