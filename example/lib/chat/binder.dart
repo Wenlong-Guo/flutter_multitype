1948deccad5350d1a9e7f5efa48c0c035a66877a
Gavin <guowenlong20000@sina.com>
2022-07-06 18:27:35 +0800

    add red packet view binder done

--- example/lib/chat/binder.dart
+++ example/lib/chat/binder.dart
@@ -164,7 +164,7 @@ class ImageOtherViewBinder extends ItemViewBinder<ChatMessage> {
                 ),
                 child: Container(
                     margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
-                    alignment: Alignment.topRight,
+                    alignment: Alignment.topLeft,
                     decoration: const BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(8.0)),
                     ),
@@ -180,20 +180,41 @@ class RedPacketMeViewBinder extends ItemViewBinder<ChatMessage> {
   @override
   Widget buildWidget(BuildContext context, ChatMessage item, int index) {
     return InkWell(
-      onTap: () {
-        EasyLoading.showToast("点击了 ${item.content}");
-        // onItemTap(context, item, index);
-      },
-      child: Container(
-          margin: const EdgeInsets.all(12),
-          height: 40,
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "${item.content}",
-            style: const TextStyle(fontSize: 20, color: Colors.white),
-            textAlign: TextAlign.left,
-          )),
-    );
+        onTap: () {
+          EasyLoading.showToast("点击了 ${item.content}");
+          // onItemTap(context, item, index);
+        },
+        child: IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minWidth: 100.0,
+                  minHeight: 100.0,
+                  maxWidth: 200.0,
+                  maxHeight: 200.0,
+                ),
+                child: Container(
+                    margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
+                    alignment: Alignment.topRight,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    child: Image.asset("assets/images/chat/image_red_normal.png", fit: BoxFit.cover)),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  item.avatar ?? defaultAvatarUrl,
+                  width: 40,
+                  height: 40,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
 
@@ -201,20 +222,41 @@ class RedPacketOtherViewBinder extends ItemViewBinder<ChatMessage> {
   @override
   Widget buildWidget(BuildContext context, ChatMessage item, int index) {
     return InkWell(
-      onTap: () {
-        EasyLoading.showToast("点击了 ${item.content}");
-        // onItemTap(context, item, index);
-      },
-      child: Container(
-          margin: const EdgeInsets.all(12),
-          height: 40,
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "${item.content}",
-            style: const TextStyle(fontSize: 20, color: Colors.white),
-            textAlign: TextAlign.left,
-          )),
-    );
+        onTap: () {
+          EasyLoading.showToast("点击了 ${item.content}");
+          // onItemTap(context, item, index);
+        },
+        child: IntrinsicHeight(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 6, 0, 6),
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  item.avatar ?? defaultAvatarUrl,
+                  width: 40,
+                  height: 40,
+                ),
+              ),
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minWidth: 100.0,
+                  minHeight: 100.0,
+                  maxWidth: 200.0,
+                  maxHeight: 200.0,
+                ),
+                child: Container(
+                    margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
+                    alignment: Alignment.topLeft,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
+                    child: Image.asset("assets/images/chat/image_red_opened.png",fit: BoxFit.cover,)),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
